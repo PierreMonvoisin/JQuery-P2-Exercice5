31bde877094c7f627476c42c4da404ace7970698
Pierre Monvoisin <monvoisin.pierre@gmail.com>
2019-11-04 18:32:30 +0100

    Exercice terminé

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+$(function() {
+  $('input').focusin(function() {
+    $(this).css('border', '1px solid green');
+  });
+  $('input').focusout(function() {
+    $(this).css('border', '1px solid red');
+  });
+});
